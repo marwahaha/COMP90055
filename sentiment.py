54c18f76e71d5845ce39146821ce4dc01277dcfd
MochuanWang <47932895+MochuanWang@users.noreply.github.com>
2019-02-24 15:11:44 +1100

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,10 +1,13 @@
 from textblob import TextBlob
 import couchdb
-couch = couchdb.Server('http://admin:62397917@115.146.92.94:5984/')
+
+#link to couchdb
+couch = couchdb.Server('http://*****:*****@115.146.92.94:5984/')
+
+#link to specific database
 db = couch['collector2']
-#doc = {'name': 'OMG', 'content': 'wmc'}
-#db.save(doc)
-#print (doc)
+
+#calculate sentiment score for each document in the database
 for id in db:
     doc = db[id]
     try:
@@ -15,6 +18,7 @@ for id in db:
         doc['polarity'] = pol
         doc['subjectivity'] = sub
 
+        #modify the document by adding the polarity and subjectivity
         db[id] = doc
 
     except KeyError:
