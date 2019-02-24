218568b2215c730aee7b2252b82e53d164158b74
MochuanWang <47932895+MochuanWang@users.noreply.github.com>
2019-02-24 14:48:59 +1100

    Update MyStream.java

--- MyStream.java
+++ MyStream.java
@@ -18,9 +18,7 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class MyStream {
 		
-	public static void main(String[] args) throws MalformedURLException {
-		
-		//twitter account configuration
+	//twitter account configuration
     	ConfigurationBuilder cb = new ConfigurationBuilder();
     	cb.setDebugEnabled(true)
     	  .setOAuthConsumerKey("FCnsAxA5xeV9EdCMWCwu*****")
@@ -36,90 +34,91 @@ public class MyStream {
 				.password("*****")
 				.build();
 		
-		CouchDbInstance dbInstance = new StdCouchDbInstance(httpClient);
-		CouchDbConnector db = dbInstance.createConnector("collector", false);
+	CouchDbInstance dbInstance = new StdCouchDbInstance(httpClient);
+	CouchDbConnector db = dbInstance.createConnector("collector2", false);
 		
-		//method listening to stream data
-		StatusListener listener = new StatusListener() {
+	//method listening to stream data
+	StatusListener listener = new StatusListener() {
 
-            public void onStatus(Status status) {
-            	String content = status.getText();
+        	public void onStatus(Status status) {
+            		String content = status.getText();
             	
-            	Boolean NAB1 = content.contains(" NAB ");
-            	Boolean NAB2 = content.toLowerCase().contains("national australia bank");
-            	Boolean NAB3 = content.contains("@NAB");
-            	Boolean CBA1 = content.contains(" CBA ");
-            	Boolean CBA2 = content.toLowerCase().contains("commonwealth bank");
-            	Boolean CBA3 = content.contains("@CommBank ");
-            	Boolean ANZ1 = content.contains(" ANZ ");
-            	Boolean ANZ2 = content.toLowerCase().contains("australia and new zealand banking");
-            	Boolean ANZ3 = content.contains("@ANZ_AU ");
-            	Boolean WBC1 = content.contains(" WBC ");
-            	Boolean WBC2 = content.toLowerCase().contains("westpac");
+            		Boolean NAB1 = content.contains(" NAB ");
+            		Boolean NAB2 = content.toLowerCase().contains("national australia bank");
+            		Boolean NAB3 = content.contains("@NAB");
+            		Boolean CBA1 = content.contains(" CBA ");
+            		Boolean CBA2 = content.toLowerCase().contains("commonwealth bank");
+            		Boolean CBA3 = content.contains("@CommBank ");
+            		Boolean ANZ1 = content.contains(" ANZ ");
+            		Boolean ANZ2 = content.toLowerCase().contains("australia and new zealand banking");
+            		Boolean ANZ3 = content.contains("@ANZ_AU ");
+            		Boolean WBC1 = content.contains(" WBC ");
+            		Boolean WBC2 = content.toLowerCase().contains("westpac");
             	
-            	if (NAB1||NAB2|| NAB3|| CBA1|| CBA2|| CBA3|| ANZ1|| ANZ2|| ANZ3|| WBC1|| WBC2) {
-            		String id = String.valueOf(status.getId());
-            		String user = String.valueOf(status.getUser().getScreenName());
-            		String cont = String.valueOf(status.getText());
-            		String location = String.valueOf(status.getUser().getLocation());
-            		
-            		Date tweetTime = status.getCreatedAt();
-                    String[] timeSplit = tweetTime.toString().split(" ");
-                    String time = timeSplit[5] + " " + timeSplit[1] + " " + timeSplit[2];
+            		if (NAB1||NAB2|| NAB3|| CBA1|| CBA2|| CBA3|| ANZ1|| ANZ2|| ANZ3|| WBC1|| WBC2) {
+				//retrieve useful tweets attributes
+            			String id = String.valueOf(status.getId());
+            			String user = String.valueOf(status.getUser().getScreenName());
+            			String cont = String.valueOf(status.getText());
+            			String location = String.valueOf(status.getUser().getLocation());
+            			//change time format
+            			Date tweetTime = status.getCreatedAt();
+                    		String[] timeSplit = tweetTime.toString().split(" ");
+                    		String time = timeSplit[5] + " " + timeSplit[1] + " " + timeSplit[2];
                     
-                    Boolean nab = false;
-                    Boolean cba = false;
-                    Boolean anz = false;
-                    Boolean wbc = false;
+                   		Boolean nab = false;
+                    		Boolean cba = false;
+                    		Boolean anz = false;
+                   		Boolean wbc = false;
                                         
-                    if (NAB1||NAB2||NAB3) {
-                    	nab = true;
-                    }
+                    		if (NAB1||NAB2||NAB3) {
+                    			nab = true;
+                   		}
                     
-                    if (CBA1||CBA2||CBA3) {
-                    	cba = true;
-                    }
+                    		if (CBA1||CBA2||CBA3) {
+                    			cba = true;
+                    		}
                     
-                    if (ANZ1||ANZ2||ANZ3) {
-                    	anz = true;
-                    }
+                    		if (ANZ1||ANZ2||ANZ3) {
+                    			anz = true;
+                    		}
                     
-                    if (WBC1||WBC2) {
-                    	wbc = true;
-                    }
+                    		if (WBC1||WBC2) {
+                    			wbc = true;
+                   		}
             		
-            		System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText() + "   City: " + status.getUser().getLocation());
-            		try {
-            			//store in couchdb
-            			MyTweet t1 = new MyTweet(user, cont, location, time, nab, cba, anz, wbc);
-                		db.create(id, t1);
-            		}catch(org.ektorp.UpdateConflictException ue) {
+            			System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText() + "   City: " + status.getUser().getLocation());
+            			try {
+            				//store in couchdb
+            				MyTweet t1 = new MyTweet(user, cont, location, time, nab, cba, anz, wbc);
+                			db.create(id, t1);
+            			}catch(org.ektorp.UpdateConflictException ue) {
             			
+            			}
             		}
             	}
-            }
 
-            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-            }
+            	public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                	System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            	}
 
-            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-            }
+            	public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                	System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+            	}
 
-            public void onScrubGeo(long userId, long upToStatusId) {
-                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-            }
+            	public void onScrubGeo(long userId, long upToStatusId) {
+                	System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            	}
 
-            public void onException(Exception ex) {
-                ex.printStackTrace();
-            }
+            	public void onException(Exception ex) {
+                	ex.printStackTrace();
+            	}
 
-			@Override
-			public void onStallWarning(StallWarning warning) {
-				System.out.println("Got stall warning:" + warning);
+		@Override
+		public void onStallWarning(StallWarning warning) {
+			System.out.println("Got stall warning:" + warning);
 				
-			}
+		}
         };
 
         //setting filter condition
@@ -134,7 +133,5 @@ public class MyStream {
         TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
         twitterStream.addListener(listener);
         twitterStream.filter(fq);
-        
-        System.out.println("OMG");
     }
 }
